7230c02592975f046dae5fb9b226583047692429
Pruthviraj Patil <pruthviraj.patil@goapptiv.in>
2023-11-23 00:55:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,142 @@
+const fs = require('fs');
+const pdfparse = require('pdf-parse');
+const xlsx = require('xlsx');
+var str=0;
+// Function to extract text from a PDF file
+function jsonToExcel(jsonData, excelFileName) {
+  // Create a worksheet
+  const ws = xlsx.utils.aoa_to_sheet(jsonData);
+
+  // Create a workbook and add the worksheet to it
+  const wb = xlsx.utils.book_new();
+  xlsx.utils.book_append_sheet(wb, ws, 'Sheet 1');
+
+  // Write the workbook to an Excel file
+  xlsx.writeFile(wb, excelFileName);
+
+  console.log(`Excel file has been created: ${excelFileName}`);
+}
+ function extractTextFromPDF(pdfPath) {
+    pdfparse(pdfPath).then(function (data) {
+       var i = data.numpages
+      
+   var title,docNo,Dated,Remarks,GrandTotal;
+var arr=[]
+    // console.log(data.text);
+
+    for(let i=0;i<data.text.length-20;i++)
+{
+  // conditon for To:Title using the logic that is it followed by email and ends at char ']' 
+  if(data.text.substr(i,7)=="Message")
+  {
+    i=i+9
+    Remarks=data.text.substr(i,15)
+    // console.log(Remarks);
+  }
+   
+  if(data.text[i]==']')
+  {
+  var k =i;
+   for( k=i;k>=10;k--)
+   {
+    if(data.text[k]=='m')
+    {
+      break;
+    }
+   }
+   k=k+2
+   var temp=""
+   for(k=k;k<k+60;k++)
+   {
+    temp+=data.text[k]
+    if(data.text[k]==']')
+    {
+      
+      break;
+    }
+   
+   }
+   title=temp;
+  //  console.log(title);
+  }
+    //For Dated just check whether the string Dated is present or not 
+  if(data.text.substr(i,5)=="Dated")
+  {
+    Dated = data.text.substr(i,18);
+    // console.log(Dated);
+  }
+
+// Similar logic is for Doc No.
+    if(data.text.substr(i,7)=="Doc No.")
+    {
+      i=i+7
+      docNo=data.text.substr(i+1,14)
+
+      // Syntax for exporting everything to excel
+   
+
+    //   console.log(title);
+      // console.log(docNo);
+    //   console.log(Remarks);
+    //   console.log(GrandTotal);
+
+    
+    }
+     // For Grand Total it is after the string Amount in words 
+     if(data.text.substr(i,15)=="Amount in words")
+     {
+       var temp = "";
+       i=i+18;
+       while(data.text[i]!='.')
+       {
+         temp+=data.text[i]
+         i++;
+       }
+       GrandTotal=temp;
+      //   console.log(title);
+      //  console.log(docNo);
+      //  console.log(Remarks);
+      //  console.log(GrandTotal);
+      //  console.log(Dated);
+    
+   arr.push([title,docNo,Dated,Remarks,GrandTotal])
+   console.log(arr);
+ 
+     }
+    }
+
+    var jsonData12 = {
+      data: arr
+  };
+   var jsonString = JSON.stringify(jsonData12, null, 2);
+   str++;
+   const excelFileName = `output_excel${str}.xlsx`;
+   jsonToExcel(arr, excelFileName)
+
+
+    });
+    
+}
+// Function to iterate over each PDF file in a folder
+async function processPDFsInFolder(folderPath) {
+
+  // Get a list of files in the folder
+  const files = fs.readdirSync(folderPath);
+
+  // Iterate over each file
+  for (const file of files) {
+      // Check if the file has a .pdf extension
+      if (file.toLowerCase().endsWith('.pdf')) {
+          const pdfPath = `${folderPath}/${file}`;
+          
+          // Await the result of extractTextFromPDF
+          const values =  extractTextFromPDF(pdfPath);
+          // Add the values to the worksheet
+      }
+  }
+
+}
+
+// Replace 'path/to/your/pdf/folder' with the path to your folder containing PDFs
+const pdfFolder = './Folder';
+processPDFsInFolder(pdfFolder);
